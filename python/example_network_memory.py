86f1679d5c9efba9a2468ab6c1aeba4220728373
Robert Harder <robert.harder@usafa.edu>
2017-01-25 12:16:23 -0700

    netmem example

--- python/example_network_memory.py
+++ python/example_network_memory.py
@@ -15,23 +15,24 @@ def main():
 
 def example_NetworkMemory():
     loop = asyncio.get_event_loop()
-    mem1 = NetworkMemory(local_addr=("225.0.0.1", 9991), remote_addr=("225.0.0.2", 9992), multicast=True)
-    mem2 = NetworkMemory(local_addr=("225.0.0.2", 9992), remote_addr=("225.0.0.1", 9991), multicast=True)
+    mem1 = NetworkMemory(local_addr=("225.0.0.1", 9991), remote_addr=("225.0.0.1", 9991), multicast=True)
+    # mem2 = NetworkMemory(local_addr=("225.0.0.2", 9992), remote_addr=("225.0.0.1", 9991), multicast=True)
 
     def when_mem1_changes(var: NetworkMemory, name, old_val, new_val):
         print("mem1 '{}' '{}' changed. Old: {}. New: {}.".format(var, name, old_val, new_val))
 
-    def when_mem2_changes(var: NetworkMemory, name, old_val, new_val):
-        print("mem2 '{}' '{}' changed. Old: {}. New: {}.".format(var, name, old_val, new_val))
+    # def when_mem2_changes(var: NetworkMemory, name, old_val, new_val):
+    #     print("mem2 '{}' '{}' changed. Old: {}. New: {}.".format(var, name, old_val, new_val))
 
     mem1.notify(when_mem1_changes)
-    mem2.notify(when_mem2_changes)
+    # mem2.notify(when_mem2_changes)
 
     async def prompt():
         while True:
             key = input("Key? ")
-            value = input("Value? ")
-            mem1.set(key, value)
+            if key != "":
+                value = input("Value? ")
+                mem1.set(key, value)
             await asyncio.sleep(0.1)
 
     asyncio.ensure_future(prompt())
