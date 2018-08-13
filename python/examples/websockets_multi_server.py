da0dfa9f4a24d47b78baed900a84ec7b286b6526
Robert Harder <rob@iharder.net>
2018-08-13 10:23:03 -0600

    fleshed out how to have multiple handlers on one server

--- python/examples/websockets_multi_server.py
+++ python/examples/websockets_multi_server.py
@@ -19,7 +19,7 @@ import webbrowser
 import aiohttp
 from aiohttp import web
 
-from handy.websocket_server import WsServer
+from handy.websocket_server import WsServer, WsHandler, MultiRouteWsServerEXPERIMENTAL
 
 __author__ = "Robert Harder"
 __email__ = "rob@iharder.net"
@@ -28,34 +28,33 @@ __license__ = "Public Domain"
 
 def main():
     # Create servers
-    cap_srv = CapitalizeEchoServer(port=9990, route="/foo")
-    # cap_srv2 = CapitalizeEchoServer(port=9990, route="/lower", transform=str.lower)
-    # rnd_srv = RandomQuoteServer(port=9991, interval=2)
-    # tim_srv = TimeOfDayServer(port=9992)
+    server = MultiRouteWsServerEXPERIMENTAL(port=9990)
+    cap_hndlr = CapitalizeEchoHandler()
+    rnd_hdnlr = RandomQuoteHandler(interval=2)
+
+    server.add_route("/cap", cap_hndlr)
+    server.add_route("/rnd", rnd_hdnlr)
 
     # Queue their start operation
     loop = asyncio.get_event_loop()
-    loop.create_task(cap_srv.start())
-    # loop.create_task(cap_srv2.start())
-    # loop.create_task(rnd_srv.start())
-    # loop.create_task(tim_srv.start())
+    loop.create_task(server.start())
 
     # Open web pages to test them
     # webtests = [9990, 9991, 9992]
     webtests = [9990]
     for port in webtests:
-        url = "http://www.websocket.org/echo.html?location=ws://localhost:{}".format(port)
+        url = "http://www.websocket.org/echo.html?location=ws://localhost:{}/cap".format(port)
         webbrowser.open(url)
     print("Be sure to click 'Connect' on the webpages that just opened.")
 
     # Queue a simulated broadcast-to-all message
     async def _alert_all(msg, delay=0):
-        await asyncio.sleep(delay)
-        print("Sending alert:", msg)
-        msg_dict = {"alert": str(msg)}
-        await cap_srv.broadcast_json(msg_dict)
-        # await rnd_srv.broadcast_json(msg_dict)
-        # await tim_srv.broadcast_json(msg_dict)
+        while(True):
+            await asyncio.sleep(delay)
+            print("Sending alert:", msg)
+            msg_dict = {"alert": str(msg)}
+            await server.broadcast_json(msg_dict)
+
 
     loop.create_task(_alert_all("all your base are belong to us", 5))
 
@@ -64,20 +63,14 @@ def main():
         loop.run_forever()
     except KeyboardInterrupt:
         print("keyboard interrupt")
-        loop.run_until_complete(cap_srv.close())
-        # loop.run_until_complete(rnd_srv.close())
-        # loop.run_until_complete(tim_srv.close())
+        loop.run_until_complete(server.close())
 
         loop.close()
     print("loop.run_forever() must have finished")
 
 
-class CapitalizeEchoServer(WsServer):
+class CapitalizeEchoHandler(WsHandler):
     """ Echoes back to client whatever they sent, but capitalized. """
-
-    def __init__(self, transform: callable = str.upper, *kargs, **kwargs):
-        super().__init__(*kargs, **kwargs)
-        self.transform = transform
 
     async def on_websocket(self, ws: web.WebSocketResponse):
         """Identify the demo server"""
@@ -86,12 +79,12 @@ class CapitalizeEchoServer(WsServer):
 
     async def on_message(self, ws: web.WebSocketResponse, ws_msg_from_client: aiohttp.WSMessage):
         if ws_msg_from_client.type == web.WSMsgType.TEXT:
-            # cap = str(ws_msg_from_client.data).upper()
-            cap = self.transform(str(ws_msg_from_client.data))
+            cap = str(ws_msg_from_client.data).upper()
+            # cap = self.transform(str(ws_msg_from_client.data))
             await ws.send_str(cap)
 
 
-class RandomQuoteServer(WsServer):
+class RandomQuoteHandler(WsHandler):
     """ Sends a random quote to the client every so many seconds. """
     QUOTES = ["Wherever you go, there you are.",
               "80% of all statistics are made up.",
@@ -123,36 +116,36 @@ class RandomQuoteServer(WsServer):
         # print("on_websocket last line within RandomQuoteServer", self)
 
 
-class TimeOfDayServer(WsServer):
-    """ Sends a message to all clients simultaneously about time of day. """
-
-    def __init__(self, interval: float = 2, *kargs, **kwargs):
-        super().__init__(*kargs, **kwargs)
-        self.interval = interval
-        # self.task = None  # type: asyncio.Task
-
-    async def start(self, *kargs, **kwargs):
-        await super().start(*kargs, **kwargs)
-
-        async def _regular_interval():
-            # counter = 0
-            while self.running and not self.shutting_down:
-                # if counter >= 5:
-                #     await self.close()
-                #     print("closed myself", self)
-                #     break
-                if int(time.time()) % self.interval == 0:  # Only on the x second mark
-                    timestamp = "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())
-                    await self.broadcast_json({"timestamp": timestamp})
-                    # counter += 1
-                await asyncio.sleep(1)
-
-        # self.task = asyncio.create_task(_regular_interval())
-        asyncio.create_task(_regular_interval())
-
-    async def close(self):
-        # self.task.cancel()
-        await super().close()
+# class TimeOfDayHandler(WsHandler):
+#     """ Sends a message to all clients simultaneously about time of day. """
+#
+#     def __init__(self, interval: float = 2, *kargs, **kwargs):
+#         super().__init__(*kargs, **kwargs)
+#         self.interval = interval
+#         # self.task = None  # type: asyncio.Task
+#
+#     async def start(self, *kargs, **kwargs):
+#         await super().start(*kargs, **kwargs)
+#
+#         async def _regular_interval():
+#             # counter = 0
+#             while self.running and not self.shutting_down:
+#                 # if counter >= 5:
+#                 #     await self.close()
+#                 #     print("closed myself", self)
+#                 #     break
+#                 if int(time.time()) % self.interval == 0:  # Only on the x second mark
+#                     timestamp = "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())
+#                     await self.broadcast_json({"timestamp": timestamp})
+#                     # counter += 1
+#                 await asyncio.sleep(1)
+#
+#         # self.task = asyncio.create_task(_regular_interval())
+#         asyncio.create_task(_regular_interval())
+#
+#     async def close(self):
+#         # self.task.cancel()
+#         await super().close()
 
 
 if __name__ == "__main__":
--- python/handy/websocket_server.py
+++ python/handy/websocket_server.py
@@ -10,6 +10,8 @@ August 2018 - Updated for Python 3.7
 """
 import asyncio
 import weakref
+from functools import partial
+from typing import Dict
 
 import aiohttp  # pip install aiohttp
 import logging
@@ -18,190 +20,6 @@ from aiohttp import web
 __author__ = "Robert Harder"
 __email__ = "rob@iharder.net"
 __license__ = "Public Domain"
-
-#
-# class WsServerZZZ(object):
-#     def __init__(self, port: int = 8000, route: str = "/"):
-#         """
-#         Create a new WsServer that will listen on the given port and at the given route.
-#         The default port is 8000, and the default route is /, ie, by default the server
-#         will listen at http://localhost:8000/
-#
-#         :param port: The port on which to listen
-#         :param route: The route at which to listen
-#         """
-#         super().__init__()
-#         self.log = logging.getLogger(__name__ + '.' + self.__class__.__name__)
-#
-#         # Passed parameters
-#         self.port = port
-#         self.route = route
-#
-#         # Internal use
-#         # self.websockets = []  # type: [web.WebSocketResponse]
-#         # self.loop = None  # type: asyncio.AbstractEventLoop
-#         self.app = None  # type: web.Application
-#         self.site = None  # type: web.TCPSite
-#         self.runner = None  # type: web.AppRunner
-#         self._running = False  # type: bool
-#         self._shutting_down = False  # type: bool
-#         # self.srv = None  # type: asyncio.base_events.Server
-#
-#     def __str__(self):
-#         return "{}({}:{})".format(self.__class__.__name__, self.port, self.route)
-#
-#     async def start(self, port: int = None, route: str = None):
-#         """
-#         Starts the websocket server and begins listening on a given port and at
-#         a given route.  These values can be provided in the __init__() constructor
-#         or at the time start() is called.  The parameters given in start() will
-#         override whatever was provided in the constructor.
-#
-#         :param port: The port on which to listen (overrides the constructor values)
-#         :param route: The route at which to lsiten (overrides the constructor values)
-#         :return: None
-#         """
-#         self.route = route or self.route
-#         self.port = port or self.port
-#
-#         self.app = web.Application()
-#         self.app.on_shutdown.append(self._on_shutdown)
-#         self.app['websockets'] = weakref.WeakSet()
-#         self.app.router.add_get(self.route, self.websocket_handler)
-#
-#         self.runner = web.AppRunner(self.app)
-#         await self.runner.setup()
-#         self.site = web.TCPSite(self.runner, 'localhost', self.port)
-#         await self.site.start()
-#         self._running = True
-#
-#         start_msg = "{} listening on port {}".format(self.__class__.__name__, self.port)
-#         self.log.info(start_msg)
-#         print(start_msg)
-#
-#     async def close(self):
-#         """ Closes all connections to websocket clients and then shuts down the server. """
-#
-#         if not self._shutting_down:
-#             self._shutting_down = True
-#             # await self.runner.cleanup()
-#             asyncio.create_task(self.runner.cleanup())
-#         #
-#         # print("close called", self.__class__.__name__)
-#         #
-#         # print("self.runner.cleanup() queuing...")
-#         # # cleanup will in turn cause _on_shutdown to be run
-#         # # await self.runner.cleanup()
-#         # asyncio.create_task(self.runner.cleanup())
-#         # print("self.runner.cleanup queued")
-#
-#     async def _on_shutdown(self, app):
-#         """Callback for when self.runner gets cleaned up."""
-#         print("_on_shutdown", self.__class__.__name__, app, flush=True)
-#         await self.close_websockets()
-#         self._running = False
-#         print("end of _on_shutdown", flush=True)
-#         # for ws in set(app['websockets']):
-#         #     print("Closing socket", ws, flush=True)
-#         #     await ws.close(code=aiohttp.WSCloseCode.GOING_AWAY, message='Server shutdown')
-#         #     print("Closed", ws)
-#         # print("runner cleanup...")
-#         # await self.runner.cleanup()
-#         # print("runner cleaned.")
-#         # print("Exiting system...")
-#
-#         # loop = asyncio.get_event_loop()
-#         # loop.run_until_complete(asyncio.sleep(2))
-#         # loop.stop()
-#
-#         # print("_on_shutdown called)")
-#         # await self.close_websockets()
-#         # # asyncio.get_event_loop().stop()
-#
-#     @property
-#     def running(self):
-#         return self._running
-#
-#     async def close_websockets(self):
-#         print("close_websockets called")
-#         for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-#             print("Closing websocket", ws)
-#             await ws.close(code=aiohttp.WSCloseCode.GOING_AWAY, message='Server shutdown')
-#             print("Closed", ws)
-#         print("end of close_websockets", flush=True)
-#
-#     async def websocket_handler(self, request: web.BaseRequest):
-#         """
-#         Handles the incoming websocket client connection and calls on_websocket()
-#         in order to hand off control to subclasses of the server.
-#
-#         If this server is being attached to an existing web.Application(), this function
-#         may be added as a route to that app without using this class's start() and close() functions,
-#         although you can still use the close_websockets() function (and should) when closing
-#         your web.Application().
-#         """
-#         ws = web.WebSocketResponse()
-#         await ws.prepare(request)
-#
-#         # self.websockets.append(ws)
-#         self.app['websockets'].add(ws)
-#         try:
-#             await self.on_websocket(ws)
-#         finally:
-#             # if ws in self.websockets:
-#             #     self.websockets.remove(ws)
-#             self.app['websockets'].discard(ws)
-#         return ws
-#
-#     async def on_websocket(self, ws: web.WebSocketResponse):
-#         """
-#         Override this function if you want to handle new incoming websocket clients.
-#         The default behavior is to listen indefinitely for incoming messages from clients
-#         and call on_message() with each one.
-#         """
-#         while True:
-#             try:
-#                 ws_msg = await ws.receive()  # type: aiohttp.WSMessage
-#             except RuntimeError as e:  # Socket closing throws RuntimeError
-#                 # print("RuntimeError - did socket close?", e, flush=True)
-#                 break
-#             else:
-#                 # Call on_message() if it got something
-#                 try:
-#                     await self.on_message(ws=ws, ws_msg_from_client=ws_msg)
-#                 except RuntimeError as e:
-#                     # Probably the socket is closing
-#                     # Exception seen during development:
-#                     #   This event loop is already running
-#                     # print("await on_message aborted")
-#                     break
-#
-#         print("on_websocket last line", self)
-#
-#     async def on_message(self, ws: web.WebSocketResponse, ws_msg_from_client: aiohttp.WSMessage):
-#         """ Override this function to handle incoming messages from websocket clients. """
-#         pass
-#
-#     async def broadcast_json(self, msg):
-#         """ Converts msg to json and broadcasts the json data to all connected clients. """
-#
-#         if self.running:
-#             for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-#                 await ws.send_json(msg)
-#
-#     async def broadcast_text(self, msg: str):
-#         """ Broadcasts a string to all connected clients. """
-#
-#         if self.running:
-#             for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-#                 await ws.send_str(msg)
-#
-#     async def broadcast_bytes(self, msg: bytes):
-#         """ Broadcasts bytes to all connected clients. """
-#
-#         if self.running:
-#             for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-#                 await ws.send_bytes(msg)
 
 
 class WsServer(object):
@@ -287,6 +105,7 @@ class WsServer(object):
         self.app = web.Application()
         self.app['websockets'] = weakref.WeakSet()
         self.app.router.add_get(self.route, self.websocket_handler)
+        # self.app.router.add_get(self.route, lambda x: self.websocket_handler(route, x))
         self.app.on_shutdown.append(self._on_shutdown)
 
         self.runner = web.AppRunner(self.app)
@@ -364,8 +183,9 @@ class WsServer(object):
             for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
                 await ws.send_bytes(msg)
 
+
 class MultiRouteWsServerEXPERIMENTAL(object):
-    def __init__(self, port: int = 8000):
+    def __init__(self, port):
         """
         Create a new WsServer that will listen on the given port.
         The default port is 8000.
@@ -382,12 +202,14 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         self.app = None  # type: web.Application
         self.site = None  # type: web.TCPSite
         self.runner = None  # type: web.AppRunner
+        self.route_handlers = {}  # type: Dict[str, WsHandler]
 
         self._running = False  # type: bool
         self._shutting_down = False  # type: bool
 
     def __str__(self):
-        return "{}({}:{})".format(self.__class__.__name__, self.port, self.route)
+        routes = ", ".join(self.route_handlers.keys())
+        return "{}({}:({})".format(self.__class__.__name__, self.port, routes)
 
     @property
     def running(self):
@@ -414,7 +236,7 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         self._running = False
 
         print("Exiting system...")
-        loop = asyncio.get_event_loop()
+        # loop = asyncio.get_event_loop()
         # loop.stop()
         # asyncio.create_task(asyncio.sleep(1))
 
@@ -426,7 +248,7 @@ class MultiRouteWsServerEXPERIMENTAL(object):
             # ws.cancel()
             print("Closed", ws, flush=True)
 
-    async def start(self, port: int = None, route: str = None):
+    async def start(self):  # , port: int = None, route: str = None):
         """
         Starts the websocket server and begins listening on a given port and at
         a given route.  These values can be provided in the __init__() constructor
@@ -437,13 +259,18 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         :param route: The route at which to lsiten (overrides the constructor values)
         :return: None
         """
-        self.route = route or self.route
-        self.port = port or self.port
+        # self.route = route or self.route
+        # self.port = port or self.port
 
         self.app = web.Application()
-        self.app['websockets'] = weakref.WeakSet()
-        # self.app.router.add_get(self.route, self.websocket_handler)
+        self.app['websockets'] = weakref.WeakSet()  # type: web.WebSocketResponse
+        self.app['route_to_websockets'] = {}  # type: Dict[str, weakref.WeakSet]
         self.app.on_shutdown.append(self._on_shutdown)
+
+        # Connect routes
+        for route in self.route_handlers.keys():
+            self.app['route_to_websockets'][route] = weakref.WeakSet()
+            self.app.router.add_get(route, partial(self.websocket_handler, route))
 
         self.runner = web.AppRunner(self.app)
         await self.runner.setup()
@@ -451,10 +278,19 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         await self.site.start()
         self._running = True
 
-    async def add_route(self, path, handler):
-        self.app.router.add_get(path, handler)
 
-    async def websocket_handler(self, request: web.BaseRequest):
+    def add_route(self, route, handler):
+        """
+
+        :param str route:
+        :param WsHandler handler:
+        :return:
+        """
+        if self.running:
+            raise Exception("Cannot add a route after server is already running.")
+        self.route_handlers[route] = handler
+
+    async def websocket_handler(self, route: str, request: web.BaseRequest):
         """
         Handles the incoming websocket client connection and calls on_websocket()
         in order to hand off control to subclasses of the server.
@@ -466,16 +302,71 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         """
         ws = web.WebSocketResponse()
         await ws.prepare(request)
-        await ws.send_str("Connected: {}".format(str(ws)))
+        await ws.send_str("Connected from route {}: {}".format(route, str(ws)))
 
-        # self.websockets.append(ws)
         self.app['websockets'].add(ws)
+        self.app['route_to_websockets'][route].add(ws)
         try:
-            await self.on_websocket(ws)
+            await self.route_handlers[route].on_websocket(ws)
         finally:
             self.app['websockets'].discard(ws)
-        print("Departing websocket_handler in WsServer")
+            self.app['route_to_websockets'][route].discard(ws)
+        # print("Departing websocket_handler in WsServer")
         return ws
+
+    # async def on_websocket(self, ws: web.WebSocketResponse):
+    #     """
+    #     Override this function if you want to handle new incoming websocket clients.
+    #     The default behavior is to listen indefinitely for incoming messages from clients
+    #     and call on_message() with each one.
+    #     """
+    #     while self.running and not self.shutting_down:
+    #         # print("RE-ENTERING on_websocket while loop")
+    #         try:
+    #             ws_msg = await ws.receive()  # type: aiohttp.WSMessage
+    #         except RuntimeError as e:  # Socket closing throws RuntimeError
+    #             # print("RuntimeError - did socket close?", e, flush=True)
+    #             break
+    #         else:
+    #             # Call on_message() if it got something
+    #             try:
+    #                 await self.on_message(ws=ws, ws_msg_from_client=ws_msg)
+    #             except RuntimeError as e:
+    #                 # Probably the socket is closing
+    #                 # print("await on_message aborted", flush=True)
+    #                 break
+    #
+    #     # print("on_websocket last line within WsServer", self)
+
+    # async def on_message(self, ws: web.WebSocketResponse, ws_msg_from_client: aiohttp.WSMessage):
+    #     """ Override this function to handle incoming messages from websocket clients. """
+    #     pass
+
+    async def broadcast_json(self, msg, route: str = None):
+        """ Converts msg to json and broadcasts the json data to all connected clients. """
+        await self._broadcast(msg, route, web.WebSocketResponse.send_json)
+
+    async def broadcast_text(self, msg: str, route: str = None):
+        """ Broadcasts a string to all connected clients. """
+        await self._broadcast(msg, route, web.WebSocketResponse.send_str)
+
+    async def broadcast_bytes(self, msg: bytes, route: str = None):
+        """ Broadcasts bytes to all connected clients. """
+        await self._broadcast(msg, route, web.WebSocketResponse.send_bytes)
+
+    async def _broadcast(self, msg, route: str, func: callable):
+
+        sockets = set()
+        if route is None:
+            sockets = set(self.app['websockets'])
+        else:
+            sockets = set(self.app['route_to_websockets'][route])
+
+        for ws in sockets:  # type: web.WebSocketResponse
+            await func(ws, msg)
+
+
+class WsHandler(object):
 
     async def on_websocket(self, ws: web.WebSocketResponse):
         """
@@ -483,42 +374,27 @@ class MultiRouteWsServerEXPERIMENTAL(object):
         The default behavior is to listen indefinitely for incoming messages from clients
         and call on_message() with each one.
         """
-        while self.running and not self.shutting_down:
-            # print("RE-ENTERING on_websocket while loop")
-            try:
+        # while self.parent.running and not self.parent.shutting_down:
+        try:
+            while not ws.closed:
                 ws_msg = await ws.receive()  # type: aiohttp.WSMessage
-            except RuntimeError as e:  # Socket closing throws RuntimeError
-                # print("RuntimeError - did socket close?", e, flush=True)
-                break
-            else:
-                # Call on_message() if it got something
-                try:
-                    await self.on_message(ws=ws, ws_msg_from_client=ws_msg)
-                except RuntimeError as e:
-                    # Probably the socket is closing
-                    # print("await on_message aborted", flush=True)
-                    break
+                await self.on_message(ws=ws, ws_msg_from_client=ws_msg)
+        except RuntimeError as e:  # Socket closing throws RuntimeError
+            print("RuntimeError - did socket close?", e, flush=True)
+            # break
+            pass
+        # else:
+        #     # Call on_message() if it got something
+        #     try:
+        #         await self.on_message(ws=ws, ws_msg_from_client=ws_msg)
+        #     except RuntimeError as e:
+        #         # Probably the socket is closing
+        #         # print("await on_message aborted", flush=True)
+        #         # break
+        #         pass
 
         # print("on_websocket last line within WsServer", self)
 
     async def on_message(self, ws: web.WebSocketResponse, ws_msg_from_client: aiohttp.WSMessage):
         """ Override this function to handle incoming messages from websocket clients. """
         pass
-
-    async def broadcast_json(self, msg):
-        """ Converts msg to json and broadcasts the json data to all connected clients. """
-        if self.running:
-            for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-                await ws.send_json(msg)
-
-    async def broadcast_text(self, msg: str):
-        """ Broadcasts a string to all connected clients. """
-        if self.running:
-            for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-                await ws.send_str(msg)
-
-    async def broadcast_bytes(self, msg: bytes):
-        """ Broadcasts bytes to all connected clients. """
-        if self.running:
-            for ws in set(self.app['websockets']):  # type: web.WebSocketResponse
-                await ws.send_bytes(msg)
