c3ccba70157ba99bc83bd3bb3de562cbedf2d84e
Robert Harder <rharder@mitre.org>
2019-09-23 13:20:38 -0600

    now supports max number of activations per unit time

--- python/handy/async_throttled_worker.py
+++ python/handy/async_throttled_worker.py
@@ -4,7 +4,11 @@
 Running large numbers of coroutines a limited number at a time.
 """
 import asyncio
-from asyncio import Event, Queue, Future
+import datetime
+import math
+import random
+import sys
+from asyncio import Event, Queue, Future, Lock
 from typing import Coroutine, Iterable, List
 
 __author__ = "Robert Harder"
@@ -20,24 +24,81 @@ def example():
         print(f"Task-{val}, delay={delay}, end")
         return delay
 
+    async def doworkfast(val=None):
+        print(f"[{val}]", datetime.datetime.now())
+        # if val < 10:
+        # await asyncio.sleep(random.random()*1)
+        # if val == 12:
+        #     await asyncio.sleep(2)
+
     async def run():
-        async with AsyncThrottledWorker(num_workers=3) as atw:
+        # async with AsyncThrottledWorker(num_workers=3) as atw:
+        #     for i in range(10):
+        #         await atw.submit_nowait(dowork(i))
+
+        async with AsyncThrottledWorker(num_workers=3, max_hz=2, hz_timebase=datetime.timedelta(seconds=5)) as atw:
             for i in range(10):
-                await atw.submit_nowait(dowork(i))
+                await asyncio.sleep(random.random() * 2)
+                await atw.submit_nowait(doworkfast(i + 1))
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
 
 
+class ThrottledQueue(Queue):
+    """
+    Releases objects no faster than the given max_hz and hz_timebase.
+    Default is one release per second.
+    Example, if you want no faster than 3 releases every minute:
+        q = ThrottledQueue(max_hz = 3, hz_timebase = datetime.timedelta(seconds=60)
+    """
+
+    def __init__(self, max_hz: int = None, hz_timebase: datetime.timedelta = None, *kargs, **kwargs):
+        super().__init__(*kargs, **kwargs)
+        self.max_hz: int = max_hz or 1
+        if max_hz <= 0:
+            raise Exception(f"Max herz must be a positive integer (was {max_hz})")
+        self.hz_timebase: datetime.timedelta = hz_timebase or datetime.timedelta(seconds=1)
+        self._timestamps: List[datetime.datetime] = []
+        self._lock: Lock = Lock()
+
+    async def get(self):
+        async with self._lock:
+            item = await super().get()
+            time_ago = datetime.datetime.now() - self.hz_timebase
+
+            # Clean up: Dump all timestamps older than time_ago
+            while self._timestamps and self._timestamps[0] < time_ago:
+                _ = self._timestamps.pop(0)
+
+            # If we have too many gets within the "time ago", then keep delaying
+            while len(self._timestamps) >= self.max_hz:
+                delay = self._timestamps[0] - time_ago
+                await asyncio.sleep(delay.total_seconds())
+                self._timestamps.pop(0)
+
+            self._timestamps.append(datetime.datetime.now())
+        return item
+
+
 class AsyncThrottledWorker:
     __QUEUE_COMPLETED__ = "__QUEUE_COMPLETED__"
 
-    def __init__(self, num_workers: int = 3):
-        self.num_workers = num_workers
+    def __init__(self,
+                 num_workers: int = 3,
+                 max_hz: int = None,
+                 hz_timebase: datetime.timedelta = None):
+        self.num_workers: int = num_workers
+        self.max_hz: int = max_hz  # Maximum number of times to execute in an amount of time
+        self.hz_timebase: datetime.timedelta = hz_timebase
         self.todo: Queue
 
     async def __aenter__(self):
-        self.todo = Queue()
+        if self.max_hz is None:
+            self.todo = Queue()
+        else:
+            self.todo = ThrottledQueue(max_hz=self.max_hz, hz_timebase=self.hz_timebase)
+        # self._timestamps_this_second = list()
         for _ in range(self.num_workers):
             asyncio.create_task(self._worker())
         return self
